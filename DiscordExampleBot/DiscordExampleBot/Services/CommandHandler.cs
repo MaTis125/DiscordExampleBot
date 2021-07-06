a7b87f114f25ee46c652415f08b281caf209f9d7
MaTis125 <matis125.lol@gmail.com>
2021-07-06 18:22:31 +0200

    Added commandHandler.cs

---
+++ DiscordExampleBot/DiscordExampleBot/Services/CommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordExampleBot.Services
+{
+    public class CommandHandler
+    {
+        public static IServiceProvider _provider;
+        private readonly DiscordSocketClient _discord;
+        private readonly CommandService _commands;
+        private readonly IConfigurationRoot _config;
+
+        public CommandHandler(IServiceProvider provider, DiscordSocketClient discord, CommandService commands, IConfigurationRoot config)
+        {
+            _provider = provider;
+            _config = config;
+            _discord = discord;
+            _commands = commands;
+
+            _discord.Ready += OnReady;
+        }
+
+        private Task OnReady()
+        {
+            Console.WriteLine($"Connected as {_discord.CurrentUser.Username}#{_discord.CurrentUser.Discriminator}");
+            return Task.CompletedTask;
+        }
+    }
+}
