a02a46fd637d8e4c0c60f37a56f475e596ad3828
MaTis125 <matis125.lol@gmail.com>
2021-07-06 18:22:18 +0200

    Added StartuoService main start to your bot service on discord

---
+++ DiscordExampleBot/DiscordExampleBot/Services/StartupService.cs
@@ -0,0 +1,41 @@
+﻿using Discord;
+using Discord.WebSocket;
+using Discord.Commands;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace DiscordExampleBot.Services
+{
+    public class StartupService
+    {
+        public static IServiceProvider _provider;
+        private readonly DiscordSocketClient _discord;
+        private readonly CommandService _commands;
+        private readonly IConfigurationRoot _config;
+
+        public StartupService(IServiceProvider provider, DiscordSocketClient discord, CommandService commands, IConfigurationRoot config)
+        {
+            _provider = provider;
+            _config = config;
+            _discord = discord;
+            _commands = commands;
+        }
+
+        public async Task StartAsync()
+        {
+            string token = _config["tokens:discord"];
+            if(string.IsNullOrEmpty(token))
+            {
+                Console.WriteLine("Please provide your Discord token in _config.yml");
+                return;
+            }
+
+            await _discord.LoginAsync(TokenType.Bot, token);
+            await _discord.StartAsync();
+
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _provider);
+        }
+    }
+}
