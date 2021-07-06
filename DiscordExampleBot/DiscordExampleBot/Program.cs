87c29b0630f74267cf65c8b5a19a3858dee62655
MaTis125 <matis125.lol@gmail.com>
2021-07-06 18:20:35 +0200

    Added Program.cs main script

---
+++ DiscordExampleBot/DiscordExampleBot/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace DiscordExampleBot
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+           => await Bot.RunAsync(args);
+    }
+}
