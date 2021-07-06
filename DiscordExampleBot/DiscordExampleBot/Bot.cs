12f32636f4f92792375f8714b1cedae29b5d97e0
MaTis125 <matis125.lol@gmail.com>
2021-07-06 18:21:34 +0200

    added bot.cs script attendent DiscordBot Service

---
+++ DiscordExampleBot/DiscordExampleBot/Bot.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using DiscordExampleBot.Services;
+
+namespace DiscordExampleBot
+{
+    public class Bot
+    {
+        public IConfigurationRoot Configuration { get; }
+
+        public Bot(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(AppContext.BaseDirectory)
+                .AddYamlFile("_config.yml");
+            Configuration = builder.Build();
+        }
+
+        public static async Task RunAsync(string[] args)
+        {
+            var bot = new Bot(args);
+            await bot.RunAsync();
+        }
+
+        public async Task RunAsync()
+        {
+            var services = new ServiceCollection();
+            ConfigureServices(services);
+
+            var provider = services.BuildServiceProvider();
+            provider.GetRequiredService<CommandHandler>();
+
+            await provider.GetRequiredService<StartupService>().StartAsync();
+            await Task.Delay(-1);
+        }
+
+        private void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton(new DiscordSocketClient(new DiscordSocketConfig
+            {
+                LogLevel = Discord.LogSeverity.Verbose,
+                MessageCacheSize = 1000
+            }))
+                .AddSingleton(new CommandService(new CommandServiceConfig
+                {
+                    LogLevel = Discord.LogSeverity.Verbose,
+                    DefaultRunMode = RunMode.Async,
+                    CaseSensitiveCommands = false
+                }))
+                .AddSingleton<CommandHandler>()
+                .AddSingleton<StartupService>()
+                .AddSingleton(Configuration);
+        }
+    }
+}
